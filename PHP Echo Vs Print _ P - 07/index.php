03893d3e4f06ebae47828a61946bc025f4a3292d
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-27 13:54:20 +0600

    ptag-added

--- PHP Echo Vs Print _ P - 07/index.php
+++ PHP Echo Vs Print _ P - 07/index.php
@@ -1,6 +1,33 @@
 <?php 
 
-echo "yahoo", " Baba";
-
+//    $test = 1;
    
+//    $test = print("Hello"); // 1
+
+//    $test = echo("Hello"); // Error
+
+// $test = print("Hello"); // 1
+// echo "<br>";
+// echo $test;
+
+// echo "Hello","Hiis","you","Raj"; // true
+
+// print "Hello","Hiis","you","Raj"; // false
+
+// echo '<h1 class="text-color">Raj </h1>';
+
+// $name = 'Raj';
+
+// // echo "<h1>".$name."</h1>"
+
+// echo <h1>$name</h1>;
+
+
+// print "<h1>$name</h1>";
+ 
+
+
+
 ?>
+
+  
---
+++ PHP Echo Vs Print _ P - 08/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+//    $test = 1;
+   
+//    $test = print("Hello"); // 1
+
+//    $test = echo("Hello"); // Error
+
+// $test = print("Hello"); // 1
+// echo "<br>";
+// echo $test;
+
+echo("Hello","Hii","you","Raj"); 
+echo "<br>";
+echo $test;
+
+?>
+
