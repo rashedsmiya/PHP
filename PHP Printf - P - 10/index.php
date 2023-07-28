b17d4e7e5252840325e94cee1ff419118ebf9863
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-28 10:25:42 +0600

    ptag-added

--- PHP Printf - P - 10/index.php
+++ PHP Printf - P - 10/index.php
@@ -1,14 +1,38 @@
 <?php 
 
-$name = "Rajdwip";
+// $name = "rajdewip";
 
-$new_name = ucwords($name);
+// $school_name = "test1"; 
 
-echo "Hello $name_name";  
+// $name = ucwords($name);
 
-printf("Hello %s my ");
-g
+// echo "Hello ".ucwords($name);
+
+// echo "Hello ".ucwords($name);
+
+// printf("Hello %s my school %s my address %s: ", ucwords($name), ucwords($school_name), "Test 2");
+
+
+$first_name = "rajdwip";
+$last_name = "majumder";
+
+$html_block = "<div> <h1>%s</h1> <h2>%s</h2> </div>";
+
+printf($html_block, ucwords($first_name), ucwords($last_name));
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
 
 
 
