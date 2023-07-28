117d6692de82d50b0cd9ef12e4e5e22165f4b822
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-28 17:05:21 +0600

    ptag-added

---
+++  PHP Arithmetic Operators P - 12/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Arithmatic Operator
+// =================
+// +
+// -
+// *
+// /
+// %
+// **
+
+$first_num = 10;
+$second_num = 3;
+
+$num = $first_num ** $second_num; // 10 * 10 * 10
+ 
+var_dump($num);
+
+
+?>
---
+++ Printf Function In PHP _ P - 11/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$first_name = "Raj";
+$last_name = "Majumder";
+
+printf('You First Name: %25s And Last Name: %1$s', $first_name, $last_name);
+
+  
+
+?>
