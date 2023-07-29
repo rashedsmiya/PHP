1391f7f51f058836060bb992aee339d9736e4725
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-29 11:54:22 +0600

    ptag-added

---
+++ PHP OR Operator _ P - 22/index.php
@@ -0,0 +1,24 @@
+<?php
+
+// if (($a > $b) || ($a == $b)) {
+//     Tahole tumi job pabe
+// }
+
+// true || false ==== true
+
+// false || false ===== false
+
+// false || true ===== true
+
+// true || true ==== true
+
+$username = "abcd";
+
+$user_role = "editor";
+
+if ($username == 'abcd' || $user_role = "editor") {
+   echo "Hello User";
+}
+
+?>
+
