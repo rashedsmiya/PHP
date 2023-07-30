9ffaadbb75f0ad0ec65c4002908aec7992d2f182
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-30 21:07:12 +0600

    ptag-added

---
+++ PHP Switch Case Statement _ P - 33/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = "raj";
+
+switch (true) {
+    case ($name == "raj"):
+         echo "Raj";
+         break;
+    case ($name == "rajat"):
+         echo "Two";
+         break;  
+    case ($name == "suman"):
+        echo "Three";
+        break;
+    default:
+        echo "Hello";    
+
+}
+
+
+?>
