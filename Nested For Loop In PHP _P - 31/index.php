e7051df039027f88ed3e8cb1a8d50d1381a08e03
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-30 18:21:29 +0600

    ptag-added

---
+++ Nested For Loop In PHP _P - 31/index.php
@@ -0,0 +1,10 @@
+<?php
+    for ($outer_list = 1; $outer_list <= 3; $outer_list++){
+        echo "List $outer_list<br>";
+        for ($inner_list = 1; $inner_list <= 2; $inner_list++){
+            echo "------- Inner List $inner_list<br>";
+        }
+    }
+
+
+?>
---
+++ PHP Break Nested Loops _  P - 32/index.php
@@ -0,0 +1,26 @@
+<?php
+    
+      // for ($outer_list = 1; $outer_list <= 3; $outer_list++) {
+  //   echo "List $outer_list<br>";
+  //   for ($inner_list = 1; $inner_list <= 2; $inner_list++) {
+  //      echo "------Inner List $inner_list<br>";
+       
+  //     if ($outer_list == 2 && $inner_list == 1) {
+  //         break 2;
+  //      } 
+              
+  //   }
+
+  // }
+  // echo "End";
+
+
+ 
+  
+     echo "Rajdwip<br>";
+     goto web_ship;
+     echo "Majumder<br>";
+     web_ship:
+     echo "web Ship<br>";
+  
+  ?>
